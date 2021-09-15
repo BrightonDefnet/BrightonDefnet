852201aa1ee488aad5585f2711fa050d1cc5e09d
U-BSD\475373 <475373@SHS-10D9C06F2.beaverton.k12.or.us>
2021-09-15 11:42:15 -0700

    random number generator

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main()
 {
+  int randNum = 0;
   int input = 0;
-  cin >> input;
-  cout << "you entered:" << input << endl;
-  input = input + 5;
-  cout << input;
-  return 0;
+  
+  srand(time(NULL));
+
+  
+  
+  randNum = rand() % 10 + 1;
+  cout << randNum;
 }
