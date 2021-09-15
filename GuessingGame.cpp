88b73f55f6fc3312335fe098d20542e3d80334c1
U-BSD\475373 <475373@SHS-10D9C06F2.beaverton.k12.or.us>
2021-09-15 12:00:38 -0700

    basic game

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -3,13 +3,27 @@ using namespace std;
 
 int main()
 {
+  // initializations
   int randNum = 0;
   int input = 0;
-  
+  int run = 0;
   srand(time(NULL));
 
-  
-  
-  randNum = rand() % 10 + 1;
-  cout << randNum;
+  while(run == 0)
+  {
+    // receive inputs and create numbers
+    cout << endl << "enter a number between 1 and 10" << endl;      
+    cin >> input;
+    randNum = rand() % 10 + 1;
+
+    // display results
+    cout << "Random Number: " << randNum << endl << "Your Number: " << input << endl;
+
+    // compare results and stop code if they win
+    if(randNum == input)
+    {
+      cout << "You Win!" << endl;
+      run = 1;
+    }
+  }
 }
