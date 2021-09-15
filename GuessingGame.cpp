3f28d6fe43cc4bf4d288ce11b7f72294da29873c
U-BSD\475373 <475373@SHS-10D9C06F2.beaverton.k12.or.us>
2021-09-15 12:23:55 -0700

    wip

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -6,24 +6,41 @@ int main()
   // initializations
   int randNum = 0;
   int input = 0;
+  int yn = 0;
   int run = 0;
+  int guesses = 0;
   srand(time(NULL));
+
+  // generate the first random number
+  numGen();
 
   while(run == 0)
   {
     // receive inputs and create numbers
     cout << endl << "enter a number between 1 and 10" << endl;      
     cin >> input;
-    randNum = rand() % 10 + 1;
-
-    // display results
+    guesses++;
     cout << "Random Number: " << randNum << endl << "Your Number: " << input << endl;
 
-    // compare results and stop code if they win
+    // compare results
+    if(randNum < input) cout << "Too High!" << endl;
+    if(randNum > input) cout << "Too Low!" << endl;
     if(randNum == input)
     {
-      cout << "You Win!" << endl;
-      run = 1;
+      // display results and ask if they want to restart
+      cout << "You Win!" << endl << "Guesses: " << guesses << endl << "Play Again? (0=y 1=n)" << endl;
+      cin >> yn;
+      guesses = 0;
+      numGen();
+      if(yn == 1)
+      {
+	run = 1;
+      }
     }
+  }
+
+  void numGen()
+  {
+    randNum = rand() % 10 + 1;
   }
 }
