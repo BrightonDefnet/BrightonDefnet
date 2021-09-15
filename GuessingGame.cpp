0e2acd2aa813692a06a126d5866d8f82923d015c
U-BSD\475373 <475373@SHS-10D9C06F2.beaverton.k12.or.us>
2021-09-15 11:09:58 -0700

    test

---
+++ GuessingGame.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int input = 0;
+  cin >> input;
+  cout << "you entered:" << input << endl;
+  input = input + 5;
+  cout << input;
+  return 0;
+}
